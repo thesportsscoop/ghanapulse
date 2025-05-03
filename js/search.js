d6009b6b5d5f43b21ba829ac6acb7ec2cace67e2
Eddie <sledge2g@gmail.com>
2025-05-03 12:09:40 +0000

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,5 @@
+// Placeholder for search functionality
+document.querySelector('.search-bar button').addEventListener('click', () => {
+  const query = document.querySelector('.search-bar input').value;
+  alert(`Searching for: "${query}" (This is a placeholder)`);
+});
